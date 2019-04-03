0568cadf5784727d7dc657d031d88a1f104b8d56
Gero Gerber <ggerber@apptects.de>
2019-04-03 17:17:03 +0200

    Rectored code in multiple files

---
+++ lib/flutteritunesapp.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_itunes/appstate.dart';
+import 'package:flutter_itunes/tracklist.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux_dev_tools/redux_dev_tools.dart';
+
+class FlutteriTunesApp extends StatelessWidget {
+  final DevToolsStore<AppState> _store;
+
+  FlutteriTunesApp(this._store);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider<AppState>(
+        store: _store,
+        child: MaterialApp(
+            title: 'Flutter iTunes Search',
+            theme: ThemeData(
+                primaryColor: Colors.white,
+                buttonColor: Colors.grey
+            ),
+            home: TrackList(title: 'iTunes Search')));
+  }
+}
